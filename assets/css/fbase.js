c4ea75e9cddb46d0255f1d508cb82b7f781de216
Jcassp <94863314+Jcassp@users.noreply.github.com>
2023-03-02 09:22:02 -0600

    Login and logout
    add the login, a home page with logout and email verification in register

---
+++ assets/css/fbase.js
@@ -0,0 +1,10 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCucGPZi-8AORbcY9dcJ6osMAdLt07yanc",
+    authDomain: "nanny-stick-db2e7.firebaseapp.com",
+    projectId: "nanny-stick-db2e7",
+    storageBucket: "nanny-stick-db2e7.appspot.com",
+    messagingSenderId: "223836988225",
+    appId: "1:223836988225:web:d8104242f7600d03cc37c0"
+};
+firebase.initializeApp(firebaseConfig);
